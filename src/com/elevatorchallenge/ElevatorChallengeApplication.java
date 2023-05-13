6e6314c52080048309de40e9df8c49046c32287c
NIYONSABA ALEX <alexnewzniyo@gmail.com>
2023-05-13 06:52:47 +0300

    feat: Add menu options

--- src/com/elevatorchallenge/ElevatorChallengeApplication.java
+++ src/com/elevatorchallenge/ElevatorChallengeApplication.java
@@ -12,12 +12,27 @@ import java.util.List;
  */
 
 public class ElevatorChallengeApplication {
+    //Console colors
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String WHITE_BACKGROUND= "\u001B[47m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    //Menu Options
+    public static final String MENU_OPTIONS = " 1. Show Status of all elevators \n 2. Show status of individual elevator \n " +
+            "3. Call elevator to floor \n 4. Set number of people waiting on a floor \n 5. Show all floors ";
 
     public static void main(String[] args) {
         // Get default elevator pool
         List<Elevator> elevators = ElevatorPool.createElevatorPool();
 
-        System.out.println("elevators");
+        System.out.println(ANSI_GREEN + BLACK_BACKGROUND + " WELCOME TO OUR ELEVATORS " + ANSI_RESET);
+
+        System.out.println("Enter Your Choice");
+
+        System.out.println(ANSI_GREEN + MENU_OPTIONS + ANSI_RESET);
+
         System.out.println(elevators.toString());
     }
 }

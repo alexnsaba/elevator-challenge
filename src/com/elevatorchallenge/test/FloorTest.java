eb26f8b9d330ef07d669b22aaf0e85c3e0f24996
NIYONSABA ALEX <alexnewzniyo@gmail.com>
2023-05-15 15:15:41 +0300

    feat: Add floor tests

--- src/com/elevatorchallenge/test/FloorTest.java
+++ src/com/elevatorchallenge/test/FloorTest.java
@@ -1,4 +1,14 @@
 package com.elevatorchallenge.test;
 
+import com.elevatorchallenge.service.FloorService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FloorTest {
+    @Test
+    public void testShowAllFloors() {
+        FloorService.showAllFloors();
+        assertEquals(FloorService.floors.get(1).getFloorName(), "First Floor");
+    }
 }

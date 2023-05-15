0ffd8295c129145447e264a6638ed9dc71546c52
NIYONSABA ALEX <alexnewzniyo@gmail.com>
2023-05-15 15:21:37 +0300

    feat: Add waiting list test

--- src/com/elevatorchallenge/test/WaitingListTest.java
+++ src/com/elevatorchallenge/test/WaitingListTest.java
@@ -1,4 +1,14 @@
 package com.elevatorchallenge.test;
 
+import com.elevatorchallenge.service.WaitingListService;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class WaitingListTest {
+    @Test
+    public void testSetPeopleWaitingOnFloor(){
+        WaitingListService.setPeopleWaitingOnFloor(1, 10);
+        assertFalse(WaitingListService.peopleWaitingOnAllFloors.isEmpty());
+    }
 }
